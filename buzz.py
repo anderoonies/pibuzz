c73ec81f60c4db65f284c98b708ee6ff8291fdf3
Andy Bayer <andrewbayer2016@u.northwestern.edu>
2016-01-07 19:25:43 -0600

    clean up, modularize

--- buzz.py
+++
@@ -1,8 +0,0 @@
-import RPi.GPIO as GPIO
-import time
-
-def buzz():
-	print 'firing'
-	GPIO.output(26, 1)
-	time.sleep(1)
-	GPIO.output(26, 0)
--- buzzer.py
+++ buzzer.py
@@ -1,30 +1,57 @@
-from twilio.rest import TwilioRestClient 
-import RPi.GPIO as GPIO
+# Python core
 import datetime
 import time
+
+# Raspberry Pi
+import RPi.GPIO as GPIO
+
+# Twilio
+from twilio.rest import TwilioRestClient
+
+# Time utilities
 from email.Utils import formatdate, parsedate
+
+# User-defined
 from buzz import buzz
 from secret import MY_AUTH_TOKEN, MY_ACCOUNT_SID, MY_DIGITS
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(26, GPIO.OUT)
-
+# Constants
 THIS_NUMBER = +17177734070
-ACCOUNT_SID =MY_ACCOUNT_SID
-AUTH_TOKEN = MY_AUTH_TOKEN 
- 
-client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
- 
+ACCOUNT_SID = MY_ACCOUNT_SID
+AUTH_TOKEN = MY_AUTH_TOKEN
 
-while(True):
-	current_time = time.mktime(time.localtime())
-	recent=current_time-1
-	messages = client.sms.messages.list(To=THIS_NUMBER)[0:2]
-	for message in messages:
-		while message.date_sent==None:
-			time.sleep(1)
-		date_sent=time.mktime(parsedate(message.date_sent))
-		if recent<=date_sent:
-			if message.from_ == MY_DIGITS:
-				if message.body == "open":
-					buzz() 
+def initialize_rpi():
+    """Sets up Raspberry Pi for input/output.
+    """
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(26, GPIO.OUT)
+
+def initialize_client():
+    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+
+def buzz():
+    """Buzz me in! Activates GPIO pin"""
+    GPIO.output(26, 1)
+    time.sleep(1)
+    GPIO.output(26, 0)
+
+def is_valid(message):
+    """Check if a message is valid"""
+    date_sent = time.mktime(parsedate(message.date_sent))
+    current_time = time.mktime(time.localtime())
+    recent_time = current_time - 1
+
+    return ((recent_time <= date_sent) and
+             message.from_ == MY_DIGITS)
+
+def listen():
+    while(True):
+        message = client.sms.messages.list(To=THIS_NUMBER)[0]
+        if is_valid(message):
+            if message.body == "open":
+                buzz()
+
+if __name__ == '__main__':
+    initialize_rpi()
+    initialize_client()
+    listen()
